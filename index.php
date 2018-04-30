be1c0d4e4e05345b54cb74a06a59b54225e50cca
Ayrton Fidelis <ayrton.vargas33@gmail.com>
2018-04-30 15:54:19 -0300

    Remove dev routes

--- index.php
+++ index.php
@@ -22,7 +22,6 @@ if (($_SERVER['CONTENT_TYPE'] ?? '') != 'application/json') {
 
 $dotenv = new Dotenv(__DIR__);
 $dotenv->load();
-
 $router = new Router(getenv('BASE_PATH'));
 
 $router->map('GET', '/council[/]*', 'Conselho\\Controllers\\Council@get', 'Conselho\\Auth@check');
@@ -74,186 +73,6 @@ $router->map('GET', '/topic_type[/]*', 'Conselho\\Controllers\\TopicType@get', '
 
 if (getenv('env') == 'dev') {
     $router->get('/dev/playground[/]*', function() {});
-    $router->get('/dev/rebuild_schemas[/]*', function() {
-        $db_client = new MongoDB\Client;
-        $db_name = getenv('DB_NAME');
-        $db = $db_client->$db_name;
-        $db->drop();
-
-        $collection_names = [
-            'council' => ['name'],
-            'evaluation' => [],
-            'grade_observation' => ['description'],
-            'grade_subject' => [],
-            'grade' => ['name'],
-            'role_type' => ['name'],
-            'role' => [],
-            'school' => ['name'],
-            'student_grade' => [],
-            'student_medical_report' => ['description'],
-            'student_observation' => ['description'],
-            'student' => ['name'],
-            'subject' => ['name'],
-            'topic_type' => ['name'],
-            'topic' => ['name'],
-            'user_token' => [],
-            'user' => ['name', 'email']
-        ];
-
-        array_walk($collection_names, function ($text_indexes, $collection_name) use ($db) {
-            $schema = json_decode(file_get_contents(BASE_PATH."/schema/$collection_name.json"));
-            try {
-                $db->createCollection($collection_name, ['validator' => ['$jsonSchema' => $schema]]);
-            } catch (\Exception $error) {
-                echo "Cannot create collection $collection_name:\n\n";
-                print_r($error);
-                exit;
-            }
-            try {
-                $text_indexes = array_map(function($field_name) {
-                    return [$field_name => 'text'];
-                }, $text_indexes);
-                $db->$collection_name->createIndex($text_indexes);
-            } catch (\Exception $error) {
-                echo "Cannot create indexes of $collection_name";
-                print_r($error);
-                exit;
-            }
-        });
-    });
-    $router->get('/dev/populate_database[/]*', function() {
-        $db_client = new MongoDB\Client;
-        $db_name = getenv('DB_NAME');
-        $db = $db_client->$db_name;
-        $now = new \MongoDB\BSON\UTCDateTime();
-
-        $school_id = $db->school->insertOne([
-            'name' => 'Mondrone',
-            'updated_at' => $now
-        ])->getInsertedId();
-
-        $user_id = $db->user->insertOne([
-            'name' => 'Ayrton',
-            'email' => 'ayrton.vargas33@gmail.com',
-            'password' => password_hash('senha', PASSWORD_DEFAULT),
-            'dev' => false,
-            'updated_at' => $now
-        ])->getInsertedId();
-
-        $db->user_token->insertOne([
-            'value' => 'pretend_its_a_token_value',
-            'user_id' => $user_id,
-            'expires_at' => $now,
-            'updated_at' => $now
-        ]);
-
-        $topic_type_id = $db->topic_type->insertOne([
-            'name' => '1 a 10',
-            'reference' => '1-10',
-            'updated_at' => $now
-        ])->getInsertedId();
-
-        $topic_id = $db->topic->insertOne([
-            'name' => 'Conversa',
-            'school_id' => $school_id,
-            'topic_type_id' => $topic_type_id,
-            'updated_at' => $now
-        ])->getInsertedId();
-
-        $council_id = $db->council->insertOne([
-            'start_date' => $now,
-            'end_date' => $now,
-            'name' => '2018 - 1',
-            'school_id' => $school_id,
-            'updated_at' => $now
-        ])->getInsertedId();
-
-        $grade_id = $db->grade->insertOne([
-            'name' => '1° A',
-            'level' => 1,
-            'school_id' => $school_id,
-            'updated_at' => $now
-        ])->getInsertedId();
-
-        $subject_id = $db->subject->insertOne([
-            'name' => 'Matemática',
-            'school_id' => $school_id,
-            'updated_at' => $now
-        ])->getInsertedId();
-
-        $role_type_id = $db->role_type->insertOne([
-            'name' => 'Professor',
-            'reference' => 'teacher',
-            'updated_at' => $now
-        ])->getInsertedId();
-
-        $role = $db->role->insertOne([
-            'user_id' => $user_id,
-            'role_type_id' => $role_type_id,
-            'school_id' => $school_id,
-            'approved' => true,
-            'updated_at' => $now
-        ])->getInsertedId();
-
-        $student_id = $db->student->insertOne([
-            'name' => 'Ayrton Vargas Witcel Fidelis',
-            'school_id' => $school_id,
-            'updated_at' => $now
-        ])->getInsertedId();
-
-        $db->student_grade->insertOne([
-            'grade_id' => $grade_id,
-            'number' => 27,
-            'student_id' => $student_id,
-            'start' => $now,
-            'updated_at' => $now
-        ]);
-
-        $db->grade_subject->insertOne([
-            'grade_id' => $grade_id,
-            'subject_id' => $subject_id,
-            'user_id' => $user_id,
-            'approved' => true,
-            'updated_at' => $now
-        ]);
-
-        $db->grade_observation->insertOne([
-            'council_id' => $council_id,
-            'user_id' => $user_id,
-            'grade_id' => $grade_id,
-            'subject_id' => $subject_id,
-            'description' => 'Turma muito conversadeira',
-            'updated_at' => $now
-        ]);
-
-        $db->student_observation->insertOne([
-            'user_id' => $user_id,
-            'student_id' => $student_id,
-            'grade_id' => $grade_id,
-            'subject_id' => $subject_id,
-            'council_id' => $council_id,
-            'description' => 'Aluno desanimado',
-            'updated_at' => $now
-        ]);
-
-        $db->student_medical_report->insertOne([
-            'student_id' => $student_id,
-            'subject_ids' => [$subject_id],
-            'description' => 'Dislexia',
-            'updated_at' => $now
-        ]);
-
-        $db->evaluation->insertOne([
-            'user_id' => $user_id,
-            'student_id' => $student_id,
-            'grade_id' => $grade_id,
-            'subject_id' => $subject_id,
-            'council_id' => $council_id,
-            'topic_id' => $topic_id,
-            'value' => 9,
-            'updated_at' => $now
-        ]);
-    });
 }
 
 try {
@@ -267,7 +86,7 @@ try {
 } catch(Exception $e) {
     // Log details...
     $router->publish("Sorry, there is an internal error, we will fix it asap!\n\n");
-    if (getenv('env') == 'dev') {
+    if (getenv('ENV') == 'dev') {
         print_r($e);
     }
 }
