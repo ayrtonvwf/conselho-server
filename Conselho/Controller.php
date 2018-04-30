59fe33c2aad80e1931513d43b2c87857263506c3
Ayrton Fidelis <ayrton.vargas33@gmail.com>
2018-04-30 16:47:27 -0300

    Remove models and update database structure

--- Conselho/Controller.php
+++ Conselho/Controller.php
@@ -1,174 +1,92 @@
 <?php
 namespace Conselho;
-use DateTime, Exception;
 use Valitron\Validator;
-use MongoDB\BSON\ObjectId;
-use Conselho\Models;
+use PDO;
 
 abstract class Controller {
-    private $token;
     private $input_data = [];
     private $validation_errors = [];
-    private $prettify = false;
-    private $default_model;
+    private $db;
     
-    public function __construct()
+    public function __construct(bool $needs_auth = true)
     {
-        $this->setup_db();
-        $this->default_model = '\\Conselho\\Models\\'.get_called_class();
-
-        $this->token = $_SERVER['HTTP_TOKEN'] ?? null;
-        $this->input_data = json_decode(file_get_contents('php://input'), true) ?? [];
-
-        $this->prettify = (bool)($_SERVER['HTTP_PRETTIFY'] ?? false);
+        if ($needs_auth) {
+            $this->check_auth();
+        }
+        $this->input_data = $this->get_input_data();
     }
 
-    protected function get_default_model() : string {
-        return $this->default_model;
+    private function check_auth() : bool {
+        return (bool) $this->get_user();
     }
 
-    private function setup_db() : void {
-        $db_name = getenv('DB_NAME');
-        \Purekid\Mongodm\MongoDB::setConfigBlock($db_name, [
-            'connection' => [
-                'hostnames' => 'localhost',
-                'database' => $db_name,
-                'options' => []
-            ]
-        ]);
+    private function get_input_data() : array {
+        return json_decode(file_get_contents('php://input'), true) ?? [];
+    }
+
+    protected function get_db_connection() : PDO {
+        if (!$this->db) {
+            $db_host = getenv('DB_HOST');
+            $db_name = getenv('DB_NAME');
+            $db_dns = "mysql:host=$db_host;dbname=$db_name;charset=UTF8";
+            $db_user = getenv('DB_USER');
+            $db_pass = getenv('DB_PASS');
+            $this->db = new PDO($db_dns, $db_user, $db_pass);
+        }
+        return $this->db;
     }
 
     protected function get_pagination(int $limit = 50) : array {
-        $page = (int) $this->input('page');
-        $skip = $limit*$page-1;
+        $page = (int) $this->input_int('page');
+        $offset = $limit*$page-1;
         return [
             'limit' => $limit,
-            'skip' => $skip
+            'offset' => $offset
         ];
     }
 
-    protected function sanitize_output($output) : array {
-        $output = (array) $output;
-
-        $output = array_map(function($value) {
-           if (is_array($value)) {
-               return $this->sanitize_output($value);
-           }
-           if ($value instanceof ObjectId) {
-               return (string) $value;
-           }
-           if ($value instanceof DateTime) {
-               return $this->datetime_to_string($value);
-           }
-           return $value;
-        }, $output);
-
-        if (isset($output['_id'])) {
-            $output['id'] = $output['_id'];
-            $id = new ObjectId($output['id']);
-            $output['created_at'] = date('Y-m-d H:i:s', $id->getTimestamp());
-            unset($output['id']);
-        }
-
-        return $output;
-    }
-
-    protected function datetime_to_string(DateTime $date) : string {
-        $format = 'Y-m-d';
-        if ($date > new DateTime(date('Y-m-d'))) {
-            $format .= ' H:i:s';
-        }
-        return $date->format($format);
-    }
-
     protected function prettify() : ?int {
-        return $this->prettify ? JSON_PRETTY_PRINT : null;
+        return !empty($_SERVER['HTTP_PRETTIFY']) ? JSON_PRETTY_PRINT : null;
     }
     
     protected function input_raw(string $key) : ?string {
         return $this->input_data[$key] ?? null;
     }
 
-    protected function input(string $key) : ?string {
+    protected function input_string(string $key) : ?string {
         $value = $this->input_raw($key);
 
         return !is_null($value) ? trim(strip_tags($value)) : null;
     }
 
-    protected function input_id(string $key) : ?ObjectId {
+    protected function input_int(string $key) : ?int {
         $value = $this->input_raw($key);
-
-        if (!$value) {
-            return null;
-        }
-
-        try {
-            $object_id = new ObjectId($value);
-        } catch (Exception $error) {
-            return null;
-        }
-
-        return $object_id;
+        return !is_null($value) ? (int) $value : null;
     }
 
-    protected function input_date(string $key) : ?DateTime {
-        $value = $this->input_raw($key);
-
-        if (!$value) {
-            return null;
-        }
-
-        try {
-            $date = new DateTime($value);
-        } catch (Exception $error) {
-            return null;
-        }
-
-        return $date;
-    }
-
-    protected function get_user() : Models\User {
-        $user_token = Models\UserToken::find(['value' => $this->token]);
-        return Models\User::one(['_id' => $user_token->user_id]);
+    protected function get_user() : object {
+        $sql = '
+            SELECT user.*
+            FROM user
+            INNER JOIN user_token ON
+                user_token.user_id = user.id AND
+                user_token.value = :token_value AND
+                user_token.expires_at < NOW()';
+        $pdo = $this->get_db_connection();
+        $statement = $pdo->prepare($sql);
+        $statement->bindValue(':token_value', $this->get_token(), PDO::PARAM_STR);
+        return $statement->fetch(PDO::FETCH_OBJ);
     }
 
     protected function get_token() : ?string {
-        return $this->token;
+        return $_SERVER['HTTP_TOKEN'] ?? null;
     }
 
     protected function run_validation(array $rules) : bool {
         $data = array_map('strip_tags', $this->input_data);
         $data = array_map('trim', $data);
 
-        $default_model = $this->default_model;
-
         $validator = new Validator($data);
-
-        $validator->addRule('objectId', function($field, $objectId) {
-            try {
-                new ObjectId($objectId);
-                return true; // no error on parsing
-            } catch (\Exception $e) {
-                return false; // cannot parse object id
-            }
-        }, '{field} must be a valid ObjectID');
-
-        $validator->addRule('inCollection', function($field, $objectId, array $params) use ($default_model) {
-            $model = "\\Council\\Models\\$params[0]" ?? $default_model;
-            $search_criteria = ['_id' => new ObjectId($objectId)];
-            return (bool) $model::one($search_criteria);
-        }, '{field} not found in database');
-
-        $validator->addRule('notInCollection', function($field, $value, array $params) use ($default_model) {
-            $model = "\\Council\\Model\\$params[0]" ?? $default_model;
-            $except_for = $params[1] ?? null;
-            $search_criteria = [$field => $value];
-            if (!$found = $model::one($search_criteria)) {
-                return true;
-            }
-            return $except_for && $found->_id == $except_for;
-        }, '{field} already exists in database');
-
         $validator->mapFieldsRules($rules);
         $validator->validate();
         $this->validation_errors = $validator->errors();
