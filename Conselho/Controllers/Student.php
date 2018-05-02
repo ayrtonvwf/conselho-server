c6fc2e78bdb86ae6ec4132ab22bf68f0b890bffd
Ayrton Fidelis <ayrton.vargas33@gmail.com>
2018-05-02 17:50:46 -0300

    Fix student controller

--- Conselho/Controllers/Student.php
+++ Conselho/Controllers/Student.php
@@ -1,6 +1,7 @@
 <?php
 namespace Conselho\Controllers;
 use Conselho\Controller;
+use PDO;
 
 class Student extends Controller
 {
@@ -8,26 +9,16 @@ class Student extends Controller
         $filters = [
             'id' => $this->input_int('id'),
             'school_id' => $this->input_int('school_id'),
-            'updated_at' => []
+            'search' => $this->input_string('search'),
+            'min_updated_at' => $this->input_string('min_updated_at'),
+            'max_updated_at' => $this->input_string('max_updated_at')
         ];
-        if ($this->input('search')) {
-            $filters['$text'] = [
-                'search' => $this->input('search'),
-                'language' => 'pt'
-            ];
-        }
-        if ($min_updated_at = $this->input('min_updated_at')) {
-            $filters['updated_at']['gte'] = $min_updated_at;
-        }
-        if ($max_updated_at = $this->input('max_updated_at')) {
-            $filters['updated_at']['lte'] = $max_updated_at;
-        }
         return array_filter($filters);
     }
 
     private function get_data() : array {
         return     [
-            'name' => $this->input('name'),
+            'name' => $this->input_string('name'),
             'school_id' => $this->input_int('school_id')
         ];
     }
@@ -36,8 +27,8 @@ class Student extends Controller
 
     private function validate_get() : bool {
         $rules = [
-            'id' => ['optional', 'objectId', 'inCollection'],
-            'school_id' => ['optional', 'objectId', ['inCollection', 'school']],
+            'id' => ['optional', 'int'],
+            'school_id' => ['optional', 'int'],
             'max_updated_at'  => ['optional', ['dateFormat', 'Y-m-d']],
             'min_updated_at'  => ['optional', ['dateFormat', 'Y-m-d']],
             'search'  => ['optional', ['lengthMin', 3]],
@@ -50,7 +41,7 @@ class Student extends Controller
     private function validate_post() : bool {
         $rules = [
             'name' => ['required', 'string', ['lengthBetween', 5, 50]],
-            'school_id' => ['required', 'objectId', ['inCollection', 'school']]
+            'school_id' => ['required', 'int']
         ];
 
         return $this->run_validation($rules);
@@ -58,9 +49,9 @@ class Student extends Controller
 
     private function validate_put() : bool {
         $rules = [
-            'id' => ['required', 'objectId', 'inCollection'],
+            'id' => ['required', 'int'],
             'name' => ['optional', 'string', ['lengthBetween', 5, 50]],
-            'school_id' => ['optional', 'objectId', ['inCollection', 'school']]
+            'school_id' => ['optional', 'int']
         ];
 
         return $this->run_validation($rules);
@@ -68,7 +59,7 @@ class Student extends Controller
 
     private function validate_delete() : bool {
         $rules = [
-            'id' => ['required', 'objectId', 'inCollection']
+            'id' => ['required', 'int']
         ];
 
         return $this->run_validation($rules);
@@ -86,13 +77,53 @@ class Student extends Controller
         }
 
         $filters = $this->get_filters();
+
+        $where = [];
+        if (isset($filters['id'])) {
+            $where[] = '`id` = :id';
+        }
+        if (isset($filters['school_id'])) {
+            $where[] = '`school_id` = :school_id';
+        }
+        if (isset($filters['max_updated_at'])) {
+            $where[] = '`updated_at` <= :max_updated_at';
+        }
+        if (isset($filters['min_updated_at'])) {
+            $where[] = '`updated_at` >= :min_updated_at';
+        }
+        if (isset($filters['search'])) {
+            $where[] = '`name` LIKE %:search%';
+        }
+
+        $where = $where ? 'WHERE '.implode(' AND ', $where) : '';
+
         $pagination = $this->get_pagination();
-        $default_model = $this->get_default_model();
-        $results = $default_model::find($filters, $pagination)->toArray();
-        $results = $this->sanitize_output($results);
+
+        $sql = "SELECT * FROM `student` $where LIMIT :limit OFFSET :offset";
+        $db = $this->get_db_connection();
+        $statement = $db->prepare($sql);
+
+        $parameters = $filters + $pagination;
+        foreach ($parameters as $parameter_name => $parameter_value) {
+            $statement->bindValue(":$parameter_name", $parameter_value, is_int($parameter_value) ? PDO::PARAM_INT : PDO::PARAM_STR);
+        }
+
+        if (!$statement->execute()) {
+            http_response_code(500);
+            return json_encode(['error' => 'CANNOT_QUERY'], $this->prettify());
+        }
+
+        $results = $statement->fetchAll(PDO::FETCH_OBJ);
+        // filter output columns
+
+        $sql = "SELECT COUNT(*) AS `all_results` FROM `student` $where";
+        $statement = $db->prepare($sql);
+        $statement->execute($filters);
+        $all_results = (int) $statement->fetchObject()->all_results;
+
         $return = [
             'results' => $results,
-            'all_results' => $default_model::count($filters),
+            'all_results' => $all_results,
             'per_page' => $pagination['limit']
         ];
         return json_encode($return, $this->prettify());
@@ -108,10 +139,13 @@ class Student extends Controller
         }
 
         $data = $this->get_data();
-        $default_model = $this->get_default_model();
+        $columns = implode(', ', array_keys($data));
+        $values = ':'.implode(', :', array_keys($data));
+        $sql = "INSERT INTO `student` ($columns) VALUES ($values)";
 
-        $entity = new $default_model($data);
-        if (!$entity->save()) {
+        $db = $this->get_db_connection();
+        $statement = $db->prepare($sql);
+        if (!$statement->execute($data)) {
             http_response_code(500);
             return json_encode(['error' => 'CANNOT_INSERT'], $this->prettify());
         }
@@ -126,13 +160,24 @@ class Student extends Controller
             ], $this->prettify());
         }
 
-        $default_model = $this->get_default_model();
-        $criteria = ['id' => $this->input_int('id')];
-        $entity = $default_model::one($criteria);
+        $data = array_filter($this->get_data());
+        if (!$data) {
+            http_response_code(400);
+            return json_encode(['error' => 'EMPTY_UPDATE'], $this->prettify());
+        }
 
-        $data = $this->get_data();
+        $fields = [];
+        foreach ($data as $column => $value) {
+            $fields[] = "`$column` = :$column";
+        }
+        $set = implode(', ', $fields);
+        $sql = "UPDATE `student` SET $set WHERE `id` = :id";
 
-        if (!$entity->update($data)) {
+        $data['id'] = $this->input_int('id');
+
+        $db = $this->get_db_connection();
+        $statement = $db->prepare($sql);
+        if (!$statement->execute($data)) {
             http_response_code(500);
             return json_encode(['error' => 'CANNOT_UPDATE'], $this->prettify());
         }
@@ -147,9 +192,12 @@ class Student extends Controller
             ], $this->prettify());
         }
 
-        $default_model = $this->get_default_model();
-        $criteria = ['id' => $this->input_int('id')];
-        $entity = $default_model::one($criteria);
-        $entity->delete();
+        $sql = "DELETE `student` WHERE `id` = :id";
+        $db = $this->get_db_connection();
+        $statement = $db->prepare($sql);
+        if (!$statement->execute(['id' => $this->input_int('int')])) {
+            http_response_code(500);
+            return json_encode(['error' => 'CANNOT_DELETE'], $this->prettify());
+        }
     }
 }
