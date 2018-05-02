fa71f9884c47859cb16231b5929850fbc6960a52
Ayrton Fidelis <ayrton.vargas33@gmail.com>
2018-05-02 18:06:49 -0300

    Fixed controller medical report (was student medical report)

---
+++ Conselho/Controllers/MedicalReport.php
@@ -0,0 +1,206 @@
+<?php
+namespace Conselho\Controllers;
+use Conselho\Controller;
+use PDO;
+
+class MedicalReport extends Controller
+{
+    private function get_filters() : array {
+        $filters = [
+            'id' => $this->input_int('id'),
+            'student_id' => $this->input_int('student_id'),
+            'search' => $this->input_string('search'),
+            'min_updated_at' => $this->input_string('min_updated_at'),
+            'max_updated_at' => $this->input_string('max_updated_at')
+        ];
+        return array_filter($filters);
+    }
+
+    private function get_data() : array {
+        return [
+            'student_id' => $this->input_int('student_id'),
+            'description' => $this->input_string('description')
+        ];
+    }
+
+    // VALIDATION
+
+    private function validate_get() : bool {
+        $rules = [
+            'id' => ['optional', 'int'],
+            'student_id' => ['optional', 'int'],
+            'subject_id' => ['optional', 'int'],
+            'max_updated_at'  => ['optional', ['dateFormat', 'Y-m-d']],
+            'min_updated_at'  => ['optional', ['dateFormat', 'Y-m-d']],
+            'search'  => ['optional', ['lengthMin', 3]],
+            'page' => ['optional', 'integer', ['min', 1]]
+        ];
+
+        return $this->run_validation($rules);
+    }
+
+    private function validate_post() : bool {
+        $rules = [
+            'student_id' => ['required', 'int'],
+            'subject_id' => ['required', 'int'],
+            'description' => ['required', 'string', ['maxLength', 50]]
+        ];
+
+        return $this->run_validation($rules);
+    }
+
+    private function validate_put() : bool {
+        $rules = [
+            'id' => ['required', 'int'],
+            'student_id' => ['optional', 'int'],
+            'subject_id' => ['optional', 'int'],
+            'description' => ['optional', 'string', ['maxLength', 50]]
+        ];
+
+        return $this->run_validation($rules);
+    }
+
+    private function validate_delete() : bool {
+        $rules = [
+            'id' => ['required', 'int']
+        ];
+
+        return $this->run_validation($rules);
+    }
+
+    // METHODS
+
+    public function get() {
+        if (!$this->validate_get()) {
+            http_response_code(400);
+            return json_encode([
+                'error' => 'INVALID_INPUT',
+                'error_messages' => $this->get_validation_errors()
+            ], $this->prettify());
+        }
+
+        $filters = $this->get_filters();
+
+        $where = [];
+        if (isset($filters['id'])) {
+            $where[] = '`id` = :id';
+        }
+        if (isset($filters['student_id'])) {
+            $where[] = '`student_id` = :student_id';
+        }
+        if (isset($filters['max_updated_at'])) {
+            $where[] = '`updated_at` <= :max_updated_at';
+        }
+        if (isset($filters['min_updated_at'])) {
+            $where[] = '`updated_at` >= :min_updated_at';
+        }
+        if (isset($filters['search'])) {
+            $where[] = '`description` LIKE %:search%';
+        }
+
+        $where = $where ? 'WHERE '.implode(' AND ', $where) : '';
+
+        $pagination = $this->get_pagination();
+
+        $sql = "SELECT * FROM `medical_report` $where LIMIT :limit OFFSET :offset";
+        $db = $this->get_db_connection();
+        $statement = $db->prepare($sql);
+
+        $parameters = $filters + $pagination;
+        foreach ($parameters as $parameter_name => $parameter_value) {
+            $statement->bindValue(":$parameter_name", $parameter_value, is_int($parameter_value) ? PDO::PARAM_INT : PDO::PARAM_STR);
+        }
+
+        if (!$statement->execute()) {
+            http_response_code(500);
+            return json_encode(['error' => 'CANNOT_QUERY'], $this->prettify());
+        }
+
+        $results = $statement->fetchAll(PDO::FETCH_OBJ);
+        // filter output columns
+
+        $sql = "SELECT COUNT(*) AS `all_results` FROM `medical_report` $where";
+        $statement = $db->prepare($sql);
+        $statement->execute($filters);
+        $all_results = (int) $statement->fetchObject()->all_results;
+
+        $return = [
+            'results' => $results,
+            'all_results' => $all_results,
+            'per_page' => $pagination['limit']
+        ];
+        return json_encode($return, $this->prettify());
+    }
+
+    public function post() {
+        if (!$this->validate_post()) {
+            http_response_code(400);
+            return json_encode([
+                'error' => 'INVALID_INPUT',
+                'error_messages' => $this->get_validation_errors()
+            ], $this->prettify());
+        }
+
+        $data = $this->get_data();
+        $columns = implode(', ', array_keys($data));
+        $values = ':'.implode(', :', array_keys($data));
+        $sql = "INSERT INTO `medical_report` ($columns) VALUES ($values)";
+
+        $db = $this->get_db_connection();
+        $statement = $db->prepare($sql);
+        if (!$statement->execute($data)) {
+            http_response_code(500);
+            return json_encode(['error' => 'CANNOT_INSERT'], $this->prettify());
+        }
+    }
+
+    public function put() {
+        if (!$this->validate_put()) {
+            http_response_code(400);
+            return json_encode([
+                'error' => 'INVALID_INPUT',
+                'error_messages' => $this->get_validation_errors()
+            ], $this->prettify());
+        }
+
+        $data = array_filter($this->get_data());
+        if (!$data) {
+            http_response_code(400);
+            return json_encode(['error' => 'EMPTY_UPDATE'], $this->prettify());
+        }
+
+        $fields = [];
+        foreach ($data as $column => $value) {
+            $fields[] = "`$column` = :$column";
+        }
+        $set = implode(', ', $fields);
+        $sql = "UPDATE `medical_report` SET $set WHERE `id` = :id";
+
+        $data['id'] = $this->input_int('id');
+
+        $db = $this->get_db_connection();
+        $statement = $db->prepare($sql);
+        if (!$statement->execute($data)) {
+            http_response_code(500);
+            return json_encode(['error' => 'CANNOT_UPDATE'], $this->prettify());
+        }
+    }
+
+    public function delete() {
+        if (!$this->validate_delete()) {
+            http_response_code(400);
+            return json_encode([
+                'error' => 'INVALID_INPUT',
+                'error_messages' => $this->get_validation_errors()
+            ], $this->prettify());
+        }
+
+        $sql = "DELETE `medical_report` WHERE `id` = :id";
+        $db = $this->get_db_connection();
+        $statement = $db->prepare($sql);
+        if (!$statement->execute(['id' => $this->input_int('int')])) {
+            http_response_code(500);
+            return json_encode(['error' => 'CANNOT_DELETE'], $this->prettify());
+        }
+    }
+}
