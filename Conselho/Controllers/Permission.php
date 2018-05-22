414b0c1b465f99a30137b433ebdc6a2d533ab84e
Ayrton Fidelis <ayrton.vargas33@gmail.com>
2018-05-22 05:48:06 -0300

    Permission get

---
+++ Conselho/Controllers/Permission.php
@@ -0,0 +1,79 @@
+<?php
+namespace Conselho\Controllers;
+use Conselho\Controller;
+use Conselho\DataSource\Permission\PermissionMapper;
+
+class Permission extends Controller
+{
+    // VALIDATION
+
+    private function validate_get() : bool {
+        $rules = [
+            'id' => ['optional', 'integer'],
+            'min_created_at' => ['optional', ['dateFormat', self::DATETIME_EXTERNAL_FORMAT]],
+            'max_created_at' => ['optional', ['dateFormat', self::DATETIME_EXTERNAL_FORMAT]],
+            'min_updated_at'  => ['optional', ['dateFormat', self::DATETIME_EXTERNAL_FORMAT]],
+            'max_updated_at'  => ['optional', ['dateFormat', self::DATETIME_EXTERNAL_FORMAT]],
+            'search'  => ['optional', ['lengthMin', 3]],
+            'reference'  => ['optional', ['lengthMin', 3]],
+            'page' => ['optional', 'integer', ['min', 1]]
+        ];
+
+        return $this->run_validation($rules);
+    }
+
+    // METHODS
+
+    public function get() : string
+    {
+        if (!$this->validate_get()) {
+            http_response_code(400);
+            return json_encode([
+                'input_errors' => $this->get_validation_errors()
+            ], $this->pretty());
+        }
+
+        $atlas = $this->atlas();
+        $select = $atlas->select(PermissionMapper::CLASS);
+        if ($id = $this->input_int('id')) {
+            $select->where('id = ?', $id);
+        }
+        if ($min_created_at = $this->input_datetime('min_created_at')) {
+            $select->where('created_at >= ?', $min_created_at);
+        }
+        if ($max_created_at = $this->input_datetime('max_created_at')) {
+            $select->where('created_at <= ?', $max_created_at);
+        }
+        if ($min_updated_at = $this->input_datetime('min_updated_at')) {
+            $select->where('updated_at >= ?', $min_updated_at);
+        }
+        if ($max_updated_at = $this->input_datetime('max_updated_at')) {
+            $select->where('updated_at <= ?', $max_updated_at);
+        }
+        if ($search = $this->input_string('search')) {
+            $select->where('(name LIKE ?', "%$search%");
+            $select->orWhere('reference LIKE ?)', "%$search%");
+        }
+        if ($reference = $this->input_string('reference')) {
+            $select->where('reference = ?', $search);
+        }
+        $pagination = $this->get_pagination();
+        $select->limit($pagination['limit']);
+        $select->offset($pagination['offset']);
+        $select->cols(['*']);
+        $results = array_map(function($result) {
+            $result['created_at'] = $this->output_datetime($result['created_at']);
+            $result['updated_at'] = $this->output_datetime($result['updated_at']);
+            return $result;
+        }, $select->fetchAll());
+
+        $return = [
+            'total_results' => $select->fetchCount(),
+            'current_page' => $pagination['page'],
+            'max_results_per_page' => $pagination['limit'],
+            'results' => $results
+        ];
+        return json_encode($return, $this->pretty());
+    }
+
+}
