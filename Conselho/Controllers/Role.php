6858ad80f9eddddbada89990d573cb9d402fea8e
Ayrton Fidelis <ayrton.vargas33@gmail.com>
2018-05-23 06:45:25 -0300

    Refactor Role controller

--- Conselho/Controllers/Role.php
+++ Conselho/Controllers/Role.php
@@ -1,47 +1,47 @@
 <?php
 namespace Conselho\Controllers;
 use Conselho\Controller;
-use PDO;
+use Conselho\DataSource\Role\RoleMapper;
 
 class Role extends Controller
 {
-    private function get_filters() : array {
-        $filters = [
-            'id' => $this->input_int('id'),
-            'user_id' => $this->input_int('user_id'),
-            'role_type_id' => $this->input_int('role_type_id'),
-            'school_id' => $this->input_int('school_id'),
-            'min_updated_at' => $this->input_string('min_updated_at'),
-            'max_updated_at' => $this->input_string('max_updated_at')
-        ];
-        $filters = array_filter($filters);
-        if ($this->input_bool('approved') !== null) {
-            $filters['approved'] = $this->input_bool('approved');
-        }
-        return $filters;
+    public function __construct()
+    {
+        parent::__construct(RoleMapper::class);
     }
 
-    private function get_data() : array {
+    private function get_get_data() : array {
+        $where = array_filter([
+            'role_type_id = ?' => $this->input_int('role_type_id'),
+            'user_id = ?' => $this->input_int('user_id')
+        ]);
+        if (!is_null($approved = $this->input_bool('approved'))) {
+            $where['approved = ?'] = $approved;
+        }
+        return $where;
+    }
+
+    private function get_post_data() : array {
         return [
-            'user_id' => $this->input_int('user_id'),
-            'role_type_id' => $this->input_int('role_type_id'),
-            'school_id' => $this->input_int('school_id'),
-            'approved' => $this->input_bool('approved')
+            'user_id' => $this->get_user()->id,
+            'role_type_id' => $this->input_int('role_type_id')
+        ];
+    }
+
+    private function get_patch_data() : array {
+        return [
+            'approved' => true,
+            'updated_at' => date(self::DATETIME_INTERNAL_FORMAT)
         ];
     }
 
     // VALIDATION
 
     private function validate_get() : bool {
-        $rules = [
-            'id' => ['optional', 'integer'],
-            'user_id' => ['optional', 'integer'],
-            'role_type_id' => ['optional', 'integer'],
-            'school_id' => ['optional', 'integer'],
-            'max_updated_at'  => ['optional', ['dateFormat', 'Y-m-d']],
-            'min_updated_at'  => ['optional', ['dateFormat', 'Y-m-d']],
-            'approved'  => ['optional', 'boolean'],
-            'page' => ['optional', 'integer', ['min', 1]]
+        $rules = self::DEFAULT_GET_RULES + [
+            'approved'  => ['optional', 'integer', ['in', [0, 1]]],
+            'role_type_id' => ['optional', 'integer', ['min', 1]],
+            'user_id' => ['optional', 'integer', ['min', 1]]
         ];
 
         return $this->run_validation($rules);
@@ -49,30 +49,7 @@ class Role extends Controller
 
     private function validate_post() : bool {
         $rules = [
-            'user_id' => ['required', 'integer'],
-            'role_type_id' => ['required', 'integer'],
-            'school_id' => ['required', 'integer'],
-            'approved' => ['required', 'boolean']
-        ];
-
-        return $this->run_validation($rules);
-    }
-
-    private function validate_put() : bool {
-        $rules = [
-            'id' => ['required', 'integer'],
-            'user_id' => ['optional', 'integer'],
-            'role_type_id' => ['optional', 'integer'],
-            'school_id' => ['optional', 'integer'],
-            'approved' => ['optional', 'boolean']
-        ];
-
-        return $this->run_validation($rules);
-    }
-
-    private function validate_delete() : bool {
-        $rules = [
-            'id' => ['required', 'objectId']
+            'role_type_id' => ['required', 'integer', ['min', 1]]
         ];
 
         return $this->run_validation($rules);
@@ -83,144 +60,61 @@ class Role extends Controller
     public function get() : string {
         if (!$this->validate_get()) {
             http_response_code(400);
-            return json_encode([
-                'error_code' => 'INVALID_INPUT',
-                'error_messages' => $this->get_validation_errors()
-            ], $this->pretty());
+            return $this->input_error_output();
         }
 
-        $filters = $this->get_filters();
+        $where = $this->get_get_data();
 
-        $where = [];
-        if (isset($filters['id'])) {
-            $where[] = '`id` = :id';
-        }
-        if (isset($filters['user_id'])) {
-            $where[] = '`user_id` = :user_id';
-        }
-        if (isset($filters['role_type_id'])) {
-            $where[] = '`role_type_id` = :role_type_id';
-        }
-        if (isset($filters['school_id'])) {
-            $where[] = '`school_id` = :school_id';
-        }
-        if (isset($filters['approved'])) {
-            $where[] = '`approved` = :approved';
-        }
-        if (isset($filters['max_updated_at'])) {
-            $where[] = '`updated_at` <= :max_updated_at';
-        }
-        if (isset($filters['min_updated_at'])) {
-            $where[] = '`updated_at` >= :min_updated_at';
-        }
+        $result = $this->search($where);
 
-        $where = $where ? 'WHERE '.implode(' AND ', $where) : '';
-
-        $pagination = $this->get_pagination();
-
-        $sql = "SELECT * FROM `role` $where LIMIT :limit OFFSET :offset";
-        $db = $this->get_db_connection();
-        $statement = $db->prepare($sql);
-
-        $parameters = $filters + $pagination;
-        foreach ($parameters as $parameter_name => $parameter_value) {
-            $statement->bindValue(":$parameter_name", $parameter_value, is_int($parameter_value) ? PDO::PARAM_INT : PDO::PARAM_STR);
-        }
-
-        if (!$statement->execute()) {
-            http_response_code(500);
-            return json_encode(['error_code' => 'CANNOT_QUERY'], $this->pretty());
-        }
-
-        $results = $statement->fetchAll(PDO::FETCH_OBJ);
-        // filter output columns
-
-        $sql = "SELECT COUNT(*) AS all_results FROM `role` $where";
-        $statement = $db->prepare($sql);
-        $statement->execute($filters);
-        $all_results = (int) $statement->fetchObject()->all_results;
-
-        $return = [
-            'results' => $results,
-            'all_results' => $all_results,
-            'per_page' => $pagination['limit']
-        ];
-        return json_encode($return, $this->pretty());
+        return json_encode($result, $this->pretty());
     }
 
-    public function post() : string {
+    public function post() : ?string {
         if (!$this->validate_post()) {
             http_response_code(400);
             return json_encode([
-                'error_code' => 'INVALID_INPUT',
-                'error_messages' => $this->get_validation_errors()
-            ], $this->pretty());
-        }
-        
-        $data = $this->get_data();
-        $columns = implode(', ', array_keys($data));
-        $values = ':'.implode(', :', array_keys($data));
-        $sql = "INSERT INTO role ($columns) VALUES ($values)";
-
-        $db = $this->get_db_connection();
-        $statement = $db->prepare($sql);
-        $statement = $this->bind_values($statement, $data);
-        if (!$statement->execute()) {
-            http_response_code(500);
-            return json_encode(['error_code' => 'CANNOT_INSERT'], $this->pretty());
-        }
-        return json_encode(['error_code' => null], $this->pretty());
-    }
-
-    public function put() : string {
-        if (!$this->validate_put()) {
-            http_response_code(400);
-            return json_encode([
-                'error_code' => 'INVALID_INPUT',
-                'error_messages' => $this->get_validation_errors()
+                'input_errors' => $this->get_validation_errors()
             ], $this->pretty());
         }
 
-        $data = array_filter($this->get_data());
-        if (!$data) {
-            http_response_code(400);
-            return json_encode(['error_code' => 'EMPTY_UPDATE'], $this->pretty());
-        }
-
-        $fields = [];
-        foreach ($data as $column => $value) {
-            $fields[] = "`$column` = :$column";
-        }
-        $set = implode(', ', $fields);
-        $sql = "UPDATE `role` SET $set WHERE `id` = :id";
-
-        $data['id'] = $this->input_int('id');
-
-        $db = $this->get_db_connection();
-        $statement = $db->prepare($sql);
-        if (!$statement->execute($data)) {
+        $data = $this->get_post_data();
+        if (!$record = $this->insert($data)) {
             http_response_code(500);
-            return json_encode(['error_code' => 'CANNOT_UPDATE'], $this->pretty());
+            return null;
         }
-        return json_encode(['error_code' => null], $this->pretty());
+
+        return $this->post_output($record);
     }
 
-    public function delete() : string {
-        if (!$this->validate_delete()) {
-            http_response_code(400);
-            return json_encode([
-                'error_code' => 'INVALID_INPUT',
-                'error_messages' => $this->get_validation_errors()
-            ], $this->pretty());
+    public function patch(int $id) : ?string {
+        if (!$record = $this->fetch($id)) {
+            http_response_code(404);
+            return null;
         }
 
-        $sql = "DELETE FROM `role` WHERE `id` = :id";
-        $db = $this->get_db_connection();
-        $statement = $db->prepare($sql);
-        if (!$statement->execute(['id' => $this->input_int('integer')])) {
+        $data = $this->get_patch_data();
+        $record->set($data);
+        if (!$this->atlas()->update($record)) {
             http_response_code(500);
-            return json_encode(['error_code' => 'CANNOT_DELETE'], $this->pretty());
+            return null;
         }
-        return json_encode(['error_code' => null], $this->pretty());
+
+        return $this->patch_output($record);
     }
+
+    public function delete(int $id) : void {
+        if (!$record = $this->fetch($id)) {
+            http_response_code(404);
+            return;
+        }
+
+        if (!$this->atlas()->delete($record)) {
+            http_response_code(500);
+            return;
+        }
+
+        http_response_code(204);
+    }
+
 }
