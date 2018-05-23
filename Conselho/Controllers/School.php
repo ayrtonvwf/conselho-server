fbd84e51110c97847391662891aa1d2ebd7fb40d
Ayrton Fidelis <ayrton.vargas33@gmail.com>
2018-05-23 06:57:07 -0300

    Refactor School controller

--- Conselho/Controllers/School.php
+++ Conselho/Controllers/School.php
@@ -5,6 +5,17 @@ use Conselho\DataSource\School\SchoolMapper;
 
 class School extends Controller
 {
+    public function __construct()
+    {
+        parent::__construct(SchoolMapper::class);
+    }
+
+    private function get_get_data() : array {
+        return array_filter([
+            'name LIKE ?' => $this->input_search('search')
+        ]);
+    }
+
     // VALIDATION
 
     private function validate_get() : bool {
@@ -26,48 +37,14 @@ class School extends Controller
     public function get() : string {
         if (!$this->validate_get()) {
             http_response_code(400);
-            return json_encode([
-                'input_errors' => $this->get_validation_errors()
-            ], $this->pretty());
+            return $this->input_error_output();
         }
 
-        $atlas = $this->atlas();
-        $select = $atlas->select(SchoolMapper::CLASS);
-        if ($id = $this->input_int('id')) {
-            $select->where('id = ?', $id);
-        }
-        if ($min_created_at = $this->input_datetime('min_created_at')) {
-            $select->where('created_at >= ?', $min_created_at);
-        }
-        if ($max_created_at = $this->input_datetime('max_created_at')) {
-            $select->where('created_at <= ?', $max_created_at);
-        }
-        if ($min_updated_at = $this->input_datetime('min_updated_at')) {
-            $select->where('updated_at >= ?', $min_updated_at);
-        }
-        if ($max_updated_at = $this->input_datetime('max_updated_at')) {
-            $select->where('updated_at <= ?', $max_updated_at);
-        }
-        if ($search = $this->input_string('search')) {
-            $select->where('name LIKE ?', "%$search%");
-        }
-        $pagination = $this->get_pagination();
-        $select->limit($pagination['limit']);
-        $select->offset($pagination['offset']);
-        $select->cols(['*']);
-        $results = array_map(function($result) {
-            $result['created_at'] = $this->output_datetime($result['created_at']);
-            $result['updated_at'] = $this->output_datetime($result['updated_at']);
-            return $result;
-        }, $select->fetchAll());
+        $where = $this->get_get_data();
 
-        $return = [
-            'total_results' => $select->fetchCount(),
-            'current_page' => $pagination['page'],
-            'max_results_per_page' => $pagination['limit'],
-            'results' => $results
-        ];
-        return json_encode($return, $this->pretty());
+        $result = $this->search($where);
+
+        return json_encode($result, $this->pretty());
     }
 
 }
