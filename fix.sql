8208cfe8f21c73242f42a68fa4b30143e62e6bd6
Ayrton Fidelis <ayrton.vargas33@gmail.com>
2018-12-11 20:38:28 -0200

    Increase student_observation and grade_observation description maxlength

--- fix.sql
+++ fix.sql
@@ -21,3 +21,8 @@ WHERE id IN (
 
 -- 9:40 - 16:15 (Anita)
 --        12:20 (Catiana)
+
+-- Increase observation maxlength
+
+ALTER TABLE `student_observation` CHANGE `description` `description` VARCHAR(3000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL;
+ALTER TABLE `grade_observation` CHANGE `description` `description` VARCHAR(3000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL;
